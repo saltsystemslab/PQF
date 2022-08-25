7d9a31203633e96b3467220f4cb0b383058a86b7
Andrew Krapivin <andrewakrapivin@gmail.com>
2022-08-25 13:07:33 -0400

    Made BackyardQRContainer not have hash as the template parameter to make everything more consistent. Compiler should compile away constants anyways, right?

--- include/QRContainers.hpp
+++ include/QRContainers.hpp
@@ -22,17 +22,17 @@ namespace DynamicPrefixFilter {
 
     //TODO: Figure out some better naming here. HashNum refers to whether we are doing the first or second (0 or 1) "hash function" (so first or second bucket)
     //Consolidation bits is how many bits we use to refer to which bucket an item came from, one bit of which is used to specify the hashnum.
-    template<std::size_t NumMiniBuckets, bool HashNum, std::size_t RemainderBits = 8, std::size_t ConsolidationBits = 4>
+    template<std::size_t NumMiniBuckets, std::size_t RemainderBits, std::size_t ConsolidationFactor>
     struct BackyardQRContainer {
-        static constexpr std::size_t ConsolidationFactor = 1ull << ConsolidationBits;
+        // static constexpr std::size_t ConsolidationFactor = 1ull << ConsolidationBits;
         // std::size_t quotient; // Do we really need to store this?
         std::size_t realRemainder; //We imbue the remainder with the hash to make a bigger remainder, so this is just to store what was originally the remainder before the addition. Not used but just to be there
         std::size_t bucketIndex;
         std::size_t miniBucketIndex;
         std::uint64_t remainder;
 
-        void finishInit(std::uint64_t frontyardBucketIndex) {
-            if constexpr (HashNum) {
+        void finishInit(std::uint64_t frontyardBucketIndex, bool hashNum) {
+            if (hashNum) {
                 remainder += (frontyardBucketIndex % ConsolidationFactor) << RemainderBits; //Division and remainder *should* be optimized to bit shift, but maybe just do that manually in the future to be sure
                 bucketIndex = frontyardBucketIndex / ConsolidationFactor;
             }
@@ -44,13 +44,13 @@ namespace DynamicPrefixFilter {
             }
         }
 
-        BackyardQRContainer(std::size_t quotient, std::uint64_t remainder): /*quotient{quotient},*/ realRemainder{remainder}, miniBucketIndex{quotient%NumMiniBuckets}, remainder(remainder) {
+        BackyardQRContainer(std::size_t quotient, std::uint64_t remainder, bool hashNum): /*quotient{quotient},*/ realRemainder{remainder}, miniBucketIndex{quotient%NumMiniBuckets}, remainder(remainder) {
             std::uint64_t frontyardBucketIndex = quotient/NumMiniBuckets;
-            finishInit(frontyardBucketIndex);
+            finishInit(frontyardBucketIndex, hashNum);
         }
 
-        BackyardQRContainer(FrontyardQRContainer<NumMiniBuckets> frontQR): /*quotient{frontQR.quotient},*/ realRemainder{frontQR.remainder}, miniBucketIndex{frontQR.miniBucketIndex}, remainder{frontQR.remainder} {
-            finishInit(frontQR.bucketIndex);
+        BackyardQRContainer(FrontyardQRContainer<NumMiniBuckets> frontQR, bool hashNum): /*quotient{frontQR.quotient},*/ realRemainder{frontQR.remainder}, miniBucketIndex{frontQR.miniBucketIndex}, remainder{frontQR.remainder} {
+            finishInit(frontQR.bucketIndex, hashNum);
         }
     };
 
--- test/TestQRContainers.cpp
+++ test/TestQRContainers.cpp
@@ -6,13 +6,12 @@
 using namespace std;
 using namespace DynamicPrefixFilter;
 
-template<std::size_t NumMiniBuckets, bool HashNum>
+template<std::size_t NumMiniBuckets, bool HashNum, size_t ConsolidationFactor>
 void testQRContainers (mt19937& generator){
     constexpr size_t RemainderBits = 8;
     using FrontyardQRType = FrontyardQRContainer<NumMiniBuckets>;
-    using BackyardQRType1 = BackyardQRContainer<NumMiniBuckets, 0, RemainderBits>;
-    using BackyardQRType2 = BackyardQRContainer<NumMiniBuckets, 1, RemainderBits>;
-    constexpr size_t cfactor = BackyardQRType1::ConsolidationFactor;
+    using BackyardQRType = BackyardQRContainer<NumMiniBuckets, RemainderBits, ConsolidationFactor>;
+    constexpr size_t cfactor = ConsolidationFactor;
 
     //We are "building up" the quotient from the backyard and stuff
     uniform_int_distribution<size_t> backBucketIndexDist(1000, 10000000000ull);
@@ -41,28 +40,28 @@ void testQRContainers (mt19937& generator){
     // assert(fqr.quotient == quotient); //these here seem overly ridiculous but whatever
     assert(fqr.remainder == remainder);
 
-    BackyardQRType1 bqr1(quotient, remainder);
+    BackyardQRType bqr1(quotient, remainder, 0);
     assert(bqr1.bucketIndex == firstBackBucketIndex);
     assert(bqr1.miniBucketIndex == miniBucketIndex);
     // assert(bqr1.quotient == quotient);
     assert(bqr1.realRemainder == remainder);
     assert(bqr1.remainder == firstBackBucketRemainder);
 
-    bqr1 = BackyardQRType1(fqr);
+    bqr1 = BackyardQRType(fqr, 0);
     assert(bqr1.bucketIndex == firstBackBucketIndex);
     assert(bqr1.miniBucketIndex == miniBucketIndex);
     // assert(bqr1.quotient == quotient);
     assert(bqr1.realRemainder == remainder);
     assert(bqr1.remainder == firstBackBucketRemainder);
 
-    BackyardQRType2 bqr2(quotient, remainder);
+    BackyardQRType bqr2(quotient, remainder, 1);
     assert(bqr2.bucketIndex == secondBackBucketIndex);
     assert(bqr2.miniBucketIndex == miniBucketIndex);
     // assert(bqr2.quotient == quotient);
     assert(bqr2.realRemainder == remainder);
     assert(bqr2.remainder == secondBackBucketRemainder);
 
-    bqr2 = BackyardQRType2(fqr);
+    bqr2 = BackyardQRType(fqr, 1);
     assert(bqr2.bucketIndex == secondBackBucketIndex);
     assert(bqr2.miniBucketIndex == miniBucketIndex);
     // assert(bqr2.quotient == quotient);
@@ -76,11 +75,11 @@ int main() {
     mt19937 generator (rd());
 
     for(size_t i{0}; i < 1000; i++) {
-        testQRContainers<52, 0>(generator);
-        testQRContainers<52, 1>(generator);
-        testQRContainers<26, 0>(generator);
-        testQRContainers<26, 1>(generator);
-        testQRContainers<47, 0>(generator);
-        testQRContainers<47, 1>(generator);
+        testQRContainers<52, 0, 14>(generator);
+        testQRContainers<52, 1, 14>(generator);
+        testQRContainers<26, 0, 14>(generator);
+        testQRContainers<26, 1, 16>(generator);
+        testQRContainers<47, 0, 16>(generator);
+        testQRContainers<47, 1, 16>(generator);
     }
 }
